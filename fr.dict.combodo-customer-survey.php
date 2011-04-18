bdd23d2d1a085301f14b7389cf2b051d79c790f5
Denis Flaven <denis.flaven@combodo.com>
2011-04-18 17:06:32 +0000

    Prototype (actually only the data model) of the customer-survey module.
    SVN:combodo-customer-survey[234]

---
+++ fr.dict.combodo-customer-survey.php
@@ -0,0 +1,18 @@
+<?php
+// Copyright (C) 2011 Combodo SARL
+//
+
+/**
+ * Localized data
+ *
+ * @author      Erwan Taloc <erwan.taloc@combodo.com>
+ * @author      Romain Quetiez <romain.quetiez@combodo.com>
+ * @author      Denis Flaven <denis.flaven@combodo.com>
+ * @license     http://www.opensource.org/licenses/gpl-3.0.html LGPL
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	// TODO: translation !
+));
+?>
