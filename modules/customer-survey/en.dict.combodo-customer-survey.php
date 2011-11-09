016fff66dfe6642d32c7aed2d581df58d19e2c9b
Romain Quetiez <romain.quetiez@combodo.com>
2011-11-09 15:34:48 +0000

    Folder reorg
    SVN:trunk[896]

---
+++ modules/customer-survey/en.dict.combodo-customer-survey.php
@@ -0,0 +1,18 @@
+<?php
+// Copyright (C) 2011 Combodo SARL
+//
+
+/**
+ * Localized data
+ *
+ * @author      Erwan Taloc <erwan.taloc@combodo.com>
+ * @author      Romain Quetiez <romain.quetiez@combodo.com>
+ * @author      Denis Flaven <denis.flaven@combodo.com>
+ * @license     http://www.opensource.org/licenses/gpl-3.0.html LGPL
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'Survey-Title' => 'iTop Customer Survey',
+));
+?>
