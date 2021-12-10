4bea4ba1ba13e41251f4728f9b4d28ecf402ea04
acognet <anne-catherine.cognet@combodo.com>
2021-12-10 09:05:25 +0100

    Update version to 2.5.1

--- module.customer-survey.php
+++ module.customer-survey.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'customer-survey/2.5.0',
+	'customer-survey/2.5.1',
 	array(
 		// Identification
 		//
