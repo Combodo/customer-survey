6ec5e25f7f718fdd2de27c97b63149c8f6a3025b
Pierre Goiffon <pierre.goiffon@combodo.com>
2023-01-04 17:57:29 +0100

    Fix include warning on InstalledVersions.php Woops forgot to do `composer install -a` before generating the autoloader :/

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Combodo\\iTop\\CustomerSurvey\\Test' => array($baseDir . '/test'),
 );
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'combodo/customer-survey',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '430872d7801566e6f72732c521fbbf5f160de2e9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'combodo/customer-survey' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '430872d7801566e6f72732c521fbbf5f160de2e9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
