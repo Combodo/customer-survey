8199e48032ca35427895e518a1dbca44818a0152
jbostoen <6421683+jbostoen@users.noreply.github.com>
2023-02-07 20:13:13 +0100

    N°5794 - Add support for placeholders (recipient and quiz_url) in subject and body (#2)
    * Add support for placeholders (recipient, current_contact, this, quiz_url) in subject and body.
    Changes as suggested by @Hipska
    
    * :recycle: Extract methods so that they can be tested
    
    * :white_check_mark: Add test class
    
    * Rename quizz to quiz
    Thanks jbostoen !
    
    * Improve placeholder detection in AppendLinkToContent
    Thanks Hipska !
    
    * Appending Link is now done done depending on an explicit parameter
    Thanks jbostoen & hipska !
    
    * Fix ApplyParamsToContent
    
    * :bulb: Fix PHPDoc type hinting, thanks Hipska !
    
    Co-authored-by: Thomas Casteleyn <thomas.casteleyn@me.com>
    
    * Fix include warning on InstalledVersions.php
    Woops forgot to do `composer install -a` before generating the autoloader :/
    
    * Change bAutoAppendLink param use
    Thanks Hipska !
    
    ---------
    
    Co-authored-by: Pierre Goiffon <pierre.goiffon@combodo.com>
    Co-authored-by: Thomas Casteleyn <thomas.casteleyn@me.com>

---
+++ test/SurveyTest.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @copyright   Copyright (C) 2010-2019 Combodo SARL
+ * @license     https://www.combodo.com/documentation/combodo-software-license.html
+ */
+
+
+namespace Combodo\iTop\CustomerSurvey\Test;
+
+
+use Combodo\iTop\Test\UnitTest\ItopDataTestCase;
+use Dict;
+use Survey;
+
+/**
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ * @backupGlobals disabled
+ */
+class SurveyTest extends ItopDataTestCase
+{
+	public function testAppendLinkToContent() {
+		$sNoPlaceholderContent = 'No placeholder';
+		$sUrl = 'URL';
+		$sQuizLink = '<br/><a href="'.$sUrl.'">'.Dict::S('Survey-notif-linktoquizz').'</a>';
+		$sWithPlaceholderContent = 'My superb quizz URL : $quiz_url$. Fantastic !';
+
+		$this->assertSame(
+			$sNoPlaceholderContent.$sQuizLink,
+			Survey::AppendLinkToContent($sNoPlaceholderContent, $sUrl),
+			'No Placeholder'
+		);
+		$this->assertSame(
+			$sWithPlaceholderContent,
+			Survey::AppendLinkToContent($sWithPlaceholderContent, $sUrl),
+			'With Placeholder'
+		);
+	}
+
+	public function testApplyParamsToContent()
+	{
+		$oSurvey = new Survey();
+		$sApplyParamsMethodName = 'ApplyParamsToContent';
+
+		$oContact = $this->CreatePerson(0, 1);
+
+		$sNoPlaceholderContent = 'No placeholder';
+		$sUrl = 'URL';
+		$sQuizLink = '<br/><a href="'.$sUrl.'">'.Dict::S('Survey-notif-linktoquizz').'</a>';
+		$sWithPlaceholderContent = 'My superb quizz URL : $quiz_url$. Fantastic !';
+		$sWithPlaceholderContentReplaced = 'My superb quizz URL : '.$sUrl.'. Fantastic !';
+
+		$this->assertSame(
+			$sNoPlaceholderContent,
+			$this->InvokeNonPublicMethod(Survey::class, $sApplyParamsMethodName, $oSurvey,
+				[$sNoPlaceholderContent, $oContact, $sUrl, false]),
+			'AppendLink false, No Placeholder'
+		);
+		$this->assertSame(
+			$sWithPlaceholderContentReplaced,
+			$this->InvokeNonPublicMethod(Survey::class, $sApplyParamsMethodName, $oSurvey,
+				[$sWithPlaceholderContent, $oContact, $sUrl, false]),
+			'AppendLink false, With Placeholder'
+		);
+		$this->assertSame(
+			$sNoPlaceholderContent.$sQuizLink,
+			$this->InvokeNonPublicMethod(Survey::class, $sApplyParamsMethodName, $oSurvey,
+				[$sNoPlaceholderContent, $oContact, $sUrl, true]),
+			'AppendLink true, No Placeholder'
+		);
+		$this->assertSame(
+			$sWithPlaceholderContentReplaced,
+			$this->InvokeNonPublicMethod(Survey::class, $sApplyParamsMethodName, $oSurvey,
+				[$sWithPlaceholderContent, $oContact, $sUrl, true]),
+			'AppendLink true, With Placeholder'
+		);
+	}
+}
