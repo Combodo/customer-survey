64af5ef2248b7e8236967242a9c91bd21d04364d
David Bie <bdejin@hotmail.com>
2023-11-01 14:56:44 +0800

    :globe_with_meridians: Add missing translation for Chinese

--- zh_cn.dict.customer-survey.php
+++ zh_cn.dict.customer-survey.php
@@ -276,7 +276,7 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 //
 
 Dict::Add('ZH CN', 'Chinese', '简体中文', array(
-	'Class:QuizzScaleQuestion/Attribute:scale_values' => '范围值',
+	'Class:QuizzScaleQuestion/Attribute:scale_values' => '定义值',
 	'Class:QuizzScaleQuestion/Attribute:scale_values+' => '~~',
 ));
 
