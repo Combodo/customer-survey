aafa80d3c4a0fdc53eb7a4e221a096ff050575a6
XavierGR <xavier.guiboud-ribaud@combodo.com>
2025-04-16 15:14:44 +0200

    :bookmark: Upgrade module to 2.6.0

--- module.customer-survey.php
+++ module.customer-survey.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'customer-survey/2.5.6',
+	'customer-survey/2.6.0',
 	array(
 		// Identification
 		//
@@ -15,7 +15,7 @@ SetupWebPage::AddModule(
 		// Setup
 		//
 		'dependencies' => array(
-			'itop-config-mgmt/2.0.0',
+			'itop-config-mgmt/3.2.0',
 		),
 		'mandatory' => false,
 		'visible' => true,
